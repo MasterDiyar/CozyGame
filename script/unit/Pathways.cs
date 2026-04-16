4f093f01520781c6efe6515b8d0dc3ec810b9098
Diyarka <240323@astanait.edu.kz>
2026-04-16 12:24:49 +0500

    pathways class fullfilled and wall

--- script/unit/Pathways.cs
+++ script/unit/Pathways.cs
@@ -5,5 +5,32 @@ namespace Testcase.script.unit;
 public partial class Pathways : Node2D
 {
     [Export] public Vector2[] Points;
+    [Export] public bool repeatable = true;
     
+    public int CurrentPoint = 0;
+    
+    public bool HasPoints => Points != null && Points.Length > 0;
+
+    public Vector2 GetTargetPosition()
+    {
+        return !HasPoints ? GlobalPosition : ToGlobal(Points[CurrentPoint]);
+    }
+
+    public float GetAngleToTarget(Vector2 fromPos)
+    {
+        if (!HasPoints) return 0f;
+        return (GetTargetPosition() - fromPos).Angle();
+    }
+
+    public void AddIndex()
+    {
+        if (!HasPoints) return;
+        
+        if (repeatable)
+            CurrentPoint = (CurrentPoint + Points.Length) % Points.Length;
+        else {
+            if (CurrentPoint < Points.Length - 1)
+                CurrentPoint++;
+        }
+    }
 }
