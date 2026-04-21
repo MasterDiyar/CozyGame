0e5b0e020779ae2391d7d9b55713b4e0a4d6b24f
Diyarka <240323@astanait.edu.kz>
2026-04-22 03:53:25 +0500

    new effect and musketer branch

--- script/game/Game.cs
+++ script/game/Game.cs
@@ -7,7 +7,7 @@ public partial class Game : Node2D
     public void PickedType(int type)
     {
         var a =GetNode<UpgradeTree>("UI/UpgradeTree");
-        var b = (new[] {"res://scene/upgrade_tree/knifer.tscn"})[type];
+        var b = (new[] {"res://scene/upgrade_tree/knifer.tscn", "res://scene/upgrade_tree/"})[type];
         a.AddToTree(GD.Load<PackedScene>(b));
     }
     public override void _Ready()
